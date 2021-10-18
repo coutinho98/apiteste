0ddc06de88f7ab7a22e21004d287a8bfb91d6b94
Mateus Couto <mateuscouto98@outlook.com>
2021-10-18 07:59:55 -0300

    Validate Complete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,14 +4,73 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (error) {
       console.log(error);
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ["Missing id"] });
+      }
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ["User do not exist"] });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ["Missing id"],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User do not exist"],
+        });
+      }
+      await user.destroy();
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((err) => err.message),
       });
     }
   }
 }
+
 
 export default new UserController();
